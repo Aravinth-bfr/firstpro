6e37f8bf9302d38b4587f7ed7ad09fb5064c85e5
Aravinth-bfr <71180874+Aravinth-bfr@users.noreply.github.com>
2020-09-12 20:50:10 +0530

    Add files via upload

---
+++ precache-manifest.4bf98e03d388a045722988c5fb283858.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79c86e08f8b8c537530e5a325d76d7c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee9c9307c0431a3446b0",
+    "url": "/static/css/main.ee8e5967.chunk.css"
+  },
+  {
+    "revision": "4fcdf788f97bbb70cd34",
+    "url": "/static/js/2.66643f8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66643f8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee9c9307c0431a3446b0",
+    "url": "/static/js/main.15e96be0.chunk.js"
+  },
+  {
+    "revision": "0369dbeaf634e5f31882",
+    "url": "/static/js/runtime-main.9974974c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
